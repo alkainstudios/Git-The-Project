c807cfb17ffad89f0616c4a9e23121db1a3cc355
JasonAlkain <AarsZach@yahoo.com>
2017-05-10 18:30:57 -0600

    Added a Ray Casting Test Script.

---
+++ Assets/Scripts/RayCastTesting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastTesting : MonoBehaviour {
+    public int dis;
+    Ray ray;
+    RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * dis, Color.green);
+    }
+}
